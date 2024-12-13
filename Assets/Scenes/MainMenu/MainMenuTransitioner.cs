36ec3d35344014b5ecbf80cd0dd4474acc76ae1f
ading0 <143818271+ading0@users.noreply.github.com>
2024-12-13 04:34:53 -0500

    updated main menu

--- Assets/Scenes/MainMenu/MainMenuTransitioner.cs
+++ Assets/Scenes/MainMenu/MainMenuTransitioner.cs
@@ -9,16 +9,31 @@ using UnityEngine.SceneManagement;
 public class MainMenuTransitioner : MonoBehaviour
 {
     [SerializeField] private InteractableUnityEventWrapper tutorialEventWrapper;
+    [SerializeField] private InteractableUnityEventWrapper level1EventWrapper;
+    [SerializeField] private InteractableUnityEventWrapper level2EventWrapper;
 
     private void Start()
     {
         tutorialEventWrapper.WhenSelect.AddListener(GoTutorial);
+        level1EventWrapper.WhenSelect.AddListener(GoLevel1);
+        level2EventWrapper.WhenSelect.AddListener(GoLevel2);
     }
 
     private void GoTutorial()
-
     {
         Debug.Log("Going to the tutorial level.");
         SceneManager.LoadScene("TutorialLevel");
     }
+
+    private void GoLevel1()
+    {
+        Debug.Log("Going to level 1.");
+        SceneManager.LoadScene("Level1");
+    }
+    private void GoLevel2()
+    {
+        Debug.Log("Going to level 2.");
+        SceneManager.LoadScene("Level2");
+    }
+
 }
